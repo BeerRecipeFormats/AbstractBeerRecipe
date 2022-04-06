29fcbb616dd54b237ff26b2535274a3ec25507e9
Thomas Bonk <thomas.bonk@sap.com>
2022-04-06 09:32:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AbstractBeerRecipe
+
+A description of this package.
---
+++ Sources/AbstractBeerRecipe/AbstractBeerRecipe.swift
@@ -0,0 +1,6 @@
+public struct AbstractBeerRecipe {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
